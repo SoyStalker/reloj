740759a9cfa62e02e3530c88a0f6627ad500d5bf
SoyStalker <soystalker10@gmail.com>
2023-02-25 20:42:29 -0300

    se agrego mas

--- index.js
+++ index.js
@@ -9,10 +9,19 @@ const actualizarHora = () => {
 
     const time = new Date()
 
+    var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    var days = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+
+    let obtenerAño = time.getFullYear();
+    let obtenerMes = meses[time.getMonth()];
+    let obtenerDia = days[time.getDay()];
     let hora = addZeros(time.getHours());
     let min = addZeros(time.getMinutes());
     let seg = addZeros(time.getSeconds());
-
+    
+    document.querySelector("#año").textContent = obtenerAño;
+    document.querySelector("#mes").textContent = obtenerMes;
+    document.querySelector("#dia").textContent = obtenerDia;
     document.querySelector(".hora").textContent = hora;
     document.querySelector(".minutos").textContent = min;
     document.querySelector(".segundos").textContent = seg;
@@ -20,3 +29,5 @@ const actualizarHora = () => {
 
 actualizarHora() 
 setInterval(actualizarHora, 1000)
+
+let dia = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
